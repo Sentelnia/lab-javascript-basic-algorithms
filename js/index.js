e02d993886acb1317c1365f62cd9a48e8bb933da
Laura Kapitza <laura.kapitza@gmail.com>
2021-02-18 21:37:27 +0100

    iteration3

--- js/index.js
+++ js/index.js
@@ -17,3 +17,26 @@ if (hacker1.length > hacker2.length) {
   }
 
 // Iteration 3: Loops
+
+let result = '';
+for (let i = 0; i < hacker1.length; i++) {
+  result += hacker1[i];
+  if (i !== hacker1.length) {
+    result += " ";
+  }
+};
+console.log(result.toUpperCase());
+
+let result2 = '';
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  result2 += hacker2[i];
+};
+console.log(result2);
+
+if ( hacker1 > hacker2) {
+  console.log("The driver's name goes first.");
+} else if (hacker1 < hacker2) {
+  console.log("Yo, the navigator goes first definitely.");
+} else {
+  console.log("What?! You both have the same name?")
+};
